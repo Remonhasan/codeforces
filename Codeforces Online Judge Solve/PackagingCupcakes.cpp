35deba09650239b56be0e33d239a811438652313
Remon Hasan <remonhasan041@gmail.com>
2020-02-04 10:05:52 -0800

    PackagingCupcakes

---
+++ Codeforces Online Judge Solve/PackagingCupcakes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        if(n>=2)
+        {
+            long long ans;
+            ans=n/2+1;
+            cout<<ans<<endl;
+
+        }
+    }
+}
