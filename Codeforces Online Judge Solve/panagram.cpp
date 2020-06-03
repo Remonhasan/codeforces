1357dc9d00093678e9c7daa6354bdece472a6aa2
Remon Hasan <remonhasan041@gmail.com>
2020-06-03 20:02:21 +0600

    520A

---
+++ Codeforces Online Judge Solve/panagram.cpp
@@ -0,0 +1,30 @@
+/* Author: Remon Hasan
+   Problem: 520A
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    char s[105];
+    int i,lenth,cnt=0,n;
+    scanf("%d",&n);
+    scanf("%s",s);
+    for(i=0;i<n;i++){
+        if(s[i]>=65&&s[i]<=90){
+            s[i]=97+s[i]-65;
+        }
+    }
+    sort(s,s+n);
+    for(i=0;i<n-1;i++){
+        if(s[i]!=s[i+1]){
+            cnt++;
+        }
+    }
+    if(cnt==25)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
