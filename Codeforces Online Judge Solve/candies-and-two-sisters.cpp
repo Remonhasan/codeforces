0f17e91ac3c58bfe5ebb23ca7a6f98dd84dfa8c9
Remon Hasan <remonhasan041@gmail.com>
2020-06-04 19:45:11 +0600

    1335A

---
+++ Codeforces Online Judge Solve/candies-and-two-sisters.cpp
@@ -0,0 +1,25 @@
+/* Author: Remon Hasan
+   Problem: 1335A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long 
+int main ()
+{
+/*
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+*/  int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		cout<<(n-1)/2<<endl;
+
+	}
+	return 0;
+}
