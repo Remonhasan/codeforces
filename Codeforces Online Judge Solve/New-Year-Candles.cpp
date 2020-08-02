ffc2b817c031cfd08edb991d879d52c876a86a9a
Remon Hasan <remonhasan041@gmail.com>
2020-08-02 21:36:26 +0600

    Add files via upload

---
+++ Codeforces Online Judge Solve/New-Year-Candles.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,sum=0,c;
+    scanf("%d%d",&a,&b);
+    sum=a;
+    while(1){
+        c=a/b;
+        a=c+(a%b);
+        sum+=c;
+        if(a<b)
+            break;
+    }
+    printf("%d\n",sum);
+    return 0;
+}
