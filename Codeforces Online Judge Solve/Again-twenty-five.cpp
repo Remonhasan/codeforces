931d7b882cd93e441f4f8ce98e4f8ecb442e8939
Remon Hasan <remonhasan041@gmail.com>
2020-09-18 20:05:56 +0600

    630A

---
+++ Codeforces Online Judge Solve/Again-twenty-five.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#define ll long long
+using namespace std;
+
+int main ()
+{
+    int n;
+    cin>>n;
+    if(n>=2)
+    {
+        //ll res = pow(5,n);
+        cout<<25<<endl;
+    }
+    return 0;
+}
+
