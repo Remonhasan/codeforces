cc178df3e981e1ce4aa1b53416f26b32136da631
Remon Hasan <remonhasan041@gmail.com>
2020-09-18 20:20:50 +0600

    1388A

---
+++ Codeforces Online Judge Solve/CaptainFlint-and- CrewRecruitment.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n<=30)
+            cout<<"NO"<<endl;
+        else
+        {
+            cout<<"YES"<<endl;
+            if(n==36||n==40||n==44)
+            {
+                cout << 6 << ' ' << 10 << ' ' << 15 << ' ' << n - 31 << endl;
+            }
+            else
+            {
+                cout << 6 << ' ' << 10 << ' ' << 14 << ' ' << n - 30 << endl;
+
+            }
+        }
+    }
+}
