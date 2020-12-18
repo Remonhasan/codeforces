91e40b5d2d53ee21dee7a47a1085888599a29d82
Remon Hasan <remonhasan041@gmail.com>
2020-12-18 15:40:21 +0600

    div3 C

---
+++ Codeforces Online Judge Solve/unique-number.cpp
@@ -0,0 +1,70 @@
+/*
+    In the Name of Allah
+    Author: Remon Hasan
+    University of Asia Pacific
+    Problem: UVA-1230 - Binary Exponentiation
+*/
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define E "\n"
+#define pb              push_back
+typedef vector<int> v;
+typedef vector<bool>B;
+
+
+void brain()
+{
+  int x;
+  cin>>x;
+  vector<int> ans;
+  int sum = 0, last = 9;
+  while (sum < x && last > 0) {
+    ans.push_back(min(x - sum, last));
+    sum += last;
+    last--;
+  }
+  if (sum < x) {
+    cout << -1 << "\n";
+  } else {
+    reverse(ans.begin(), ans.end());
+    for (int i : ans) {
+      cout << i;
+    }
+    cout << "\n";
+  }
+
+}
+
+int main ()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+/*
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+*/
+	int t;
+	cin>>t;
+	while(t--){
+     
+     brain();
+
+	}	
+}
