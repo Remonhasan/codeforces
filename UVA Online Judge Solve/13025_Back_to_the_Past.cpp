9e1e3fbf1a56c3126079e59c808b6f88b70037a6
Remon Hasan <remonhasan041@gmail.com>
2021-01-07 19:04:51 +0600

    Add files via upload

---
+++ UVA Online Judge Solve/13025_Back_to_the_Past.cpp
@@ -0,0 +1,22 @@
+/* Name of Allah
+Author: Remon Hasan
+Problem: UVA-11614
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+/* Name of Allah
+Author: Remon Hasan
+Problem: UVA-13025
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main ()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    cout<<"May 29, 2013 Wednesday"<<endl;
+}
