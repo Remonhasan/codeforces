b0559185cca37a7f175aa5ce9515a01014a512ba
Remon Hasan <remonhasan041@gmail.com>
2021-04-10 02:49:07 +0600

    Add files via upload

---
+++ UVA Online Judge Solve/uva10469.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    unsigned int a,b;
+    while(scanf("%u %u",&a,&b)==2){
+      printf("%u\n",a^b);
+    }
+}
