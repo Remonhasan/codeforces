78ed0edfbe67e359781f728190829f3b9e865ad1
Remon Hasan <remonhasan041@gmail.com>
2021-04-11 19:08:18 +0600

    round 198

---
+++ Atcoder Contest Solve/A_Div.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+#define ll long long 
+#define ull unsigned long long
+#define pb push_back
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+
+void solve ()
+{
+   int n;
+   cin>>n;
+   cout<<n-1<<endl; 
+}
+
+int main ()
+{
+    FastRead;
+    solve(); 
+}
